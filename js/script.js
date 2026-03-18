d281bef3512290ce23ae6244fe9393e40b840081
Hatsumey <guillaumevelilla6@gmail.com>
2026-03-18 17:08:54 +0100

    Home page mobile layout pretty much ok !
    Added a js script for opacity header fade by scrolling content

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+const header = document.querySelector('.page-header');
+const aboutme = document.querySelector('#aboutme-scroll-box');
+const projects = document.querySelector('.projects-scroll');
+
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+
+
+function handleHeaderOpacity(scrollElement) {
+    if (!mediaQuery.matches) return;
+
+    const scrollTop = scrollElement.scrollTop;
+    const maxScroll = 200;
+
+    let opacity = 1 - scrollTop / maxScroll;
+    opacity = Math.max(0, Math.min(1, opacity));
+
+    header.style.opacity = opacity;
+}
+
+aboutme.addEventListener('scroll', () => {
+    handleHeaderOpacity(aboutme);
+});
+
+projects.addEventListener('scroll', () => {
+    handleHeaderOpacity(projects);
+});
