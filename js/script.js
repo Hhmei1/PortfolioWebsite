112522ca4b9f47c701d3c654445d499940f7b049
Hatsumey <guillaumevelilla6@gmail.com>
2026-03-19 19:03:00 +0100

    Header Management for Mobile Layout

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 const header = document.querySelector('.page-header');
 const aboutme = document.querySelector('#aboutme-scroll-box');
 const projects = document.querySelector('.projects-scroll');
+const isProjectPage = document.querySelector('.project-page')
 
 const mediaQuery = window.matchMedia('(max-width: 768px)');
 
@@ -11,6 +12,7 @@ const mediaQuery = window.matchMedia('(max-width: 768px)');
 
 function handleHeaderOpacity(scrollElement) {
     if (!mediaQuery.matches) return;
+    
 
     const scrollTop = scrollElement.scrollTop;
     const maxScroll = 400;
@@ -21,11 +23,41 @@ function handleHeaderOpacity(scrollElement) {
     header.style.opacity = opacity;
 }
 
-aboutme.addEventListener('scroll', () => {
-    handleHeaderOpacity(aboutme);
-});
+if (aboutme) {
 
-projects.addEventListener('scroll', () => {
+    aboutme.addEventListener('scroll', () => {
+    handleHeaderOpacity(aboutme);
+
+  });
+}
+
+if (projects) {
+    projects.addEventListener('scroll', () => {
     handleHeaderOpacity(projects);
 });
+}
 
+
+
+const navLinks = document.querySelectorAll('.nav-link');
+const menuLink = document.querySelector('.open-menu');
+
+
+
+if (isProjectPage) {
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      header.classList.add('closed');
+    });
+  });
+
+  if (menuLink) {
+    menuLink.addEventListener('click', () => {
+      header.classList.remove('closed');
+    });
+  }
+
+}
+
+console.log(navLinks);
